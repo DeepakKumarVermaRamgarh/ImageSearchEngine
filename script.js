5d4a0976ff56d10614cb6638e0491f2552128206
Deepak Kumar Verma <128968635+DeepakKumarVermaRamgarh@users.noreply.github.com>
2023-07-07 18:15:23 +0530

    adding comments

--- script.js
+++ script.js
@@ -6,26 +6,27 @@ var styleSheet = document.createElement('style');
 
 const client_id = 'BQSM3KsnKgrWRcOe2TXa--TAnqOr--9YdzorCPrsI1g';
 
-
-
 let keyword = '';
 let page = 1;
 
+// Function to search for images based on the entered keyword
 async function searchImages() {
     keyword = searchBox.value.trim();
-    const url = `https://api.unsplash.com/search/photos?page=${page}&query=${keyword}&client_id=${client_id}&per_page=12`
+    const url = `https://api.unsplash.com/search/photos?page=${page}&query=${keyword}&client_id=${client_id}&per_page=12`;
 
     const response = await fetch(url);
     const data = await response.json();
 
-    if(page === 1){
-        searchResult.innerHTML = ""
+    // Clear search results on the first page
+    if (page === 1) {
+        searchResult.innerHTML = "";
     }
 
     const results = data.results;
 
-    if(!results.length) return;
+    if (!results.length) return;
 
+    // Display the search results
     results.map(result => {
         const image = document.createElement('img');
         image.src = result.urls.small;
@@ -36,20 +37,25 @@ async function searchImages() {
 
         imageLink.appendChild(image);
         searchResult.appendChild(imageLink);
-    })
+    });
+
+    // Show the "Show more" button
     showMoreBtn.style.display = 'block';
+
+    // Add style to give credit to unsplash container
     styleSheet.innerHTML = "body::before { display: block !important; }";
     document.head.appendChild(styleSheet);
 }
 
-
+// Handle form submission to initiate the image search
 searchForm.onsubmit = (e) => {
     e.preventDefault();
     page = 1;
     searchImages();
 }
 
-showMoreBtn.onclick = function(){
+// Handle click event on the "Show more" button to load more search results
+showMoreBtn.onclick = function () {
     page++;
     searchImages();
 }
